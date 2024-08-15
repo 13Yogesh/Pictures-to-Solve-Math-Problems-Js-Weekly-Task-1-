535192cf6701752a7217a4701bfee354d4be4bab
13Yogesh <yogeshroro1306@gmail.com>
2024-08-15 23:41:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function Circle()
+{
+        const pi=3.14
+        let c1=prompt('enter radious area')
+        let c2=prompt('enter radious area')
+        let res=prompt('area of circle',pi*c1*c2)
+        console.log=('area of circle',res)
+    
+}
+
+function square()
+{
+   let a=prompt('enter the value')
+   let r1=prompt ('area of square',a*a)
+   console.log('area of square',r1)
+}
+
+function cylinder()
+{
+   
+    let c1=prompt('enter radious area')
+    let c2=prompt('enter height area')
+    let res=prompt('',c1*c1)
+    console.log=('area of cylinder',res)
+        
+}
+
+function numbers()
+{
+    let c1=prompt('enter the numbers')
+        let c2=promt('enter the numbers')
+        let res=(c1*c2)
+        console.log('area of numbers',res)
+}
+
+function alpha()
+{
+    let c1=prompt('enter letters area')
+    let c2=prompt('enter letterz area')
+    let res=prompt('',c1+c2)
+    console.log=('area of alpha',res)
+}
